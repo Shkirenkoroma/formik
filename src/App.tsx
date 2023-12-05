4c6413a6413c5766e2a72de5fa94511aefb4365b
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-12-05 18:56:24 +0300

    feature/FRM: completed project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { FC } from 'react';
+import { Field, Form, Formik } from 'formik';
+import styles from './App.module.css';
+import classnames from 'classnames';
 
-function App() {
+function validateEmail(value: string) {
+  if (!value) {
+    return 'Required'
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2, 4}$/i.test(value)) {
+    return 'Invalid email address'
+  }
+};
+
+function validatePassword(value: string) {
+  if (!value) {
+    return 'Required'
+  }
+};
+
+const App: FC = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Formik
+      initialValues={{ email: '', password: '' }}
+      onSubmit={(values) => {
+        console.log('submit', values)
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form className={styles.form}>
+          <label
+            className={classnames(styles.label, {
+              [styles.errorLaber]: errors.email && touched.email,
+            })}
+          >
+            Электронная почта
+          </label>
+          <Field
+            className={classnames(styles.field, {
+              [styles.errorInput]: errors.email && touched.email,
+            })}
+            name="email"
+            type="email"
+            validate={validateEmail}
+          />
+          {errors.email && touched.email && (
+            <div className={styles.error}>{errors.email}</div>
+          )}
+          <label className={classnames(styles.label)}>Пароль</label>
+          <Field
+            className={classnames(styles.field, {
+              [styles.errorInput]: errors.password && touched.password,
+            })}
+            name="password"
+            type="password"
+            validate={validatePassword}
+          />
+          {errors.password && touched.password && (
+            <div className={styles.error}>{errors.password}</div>
+          )}
+          <button className={styles.button} type="submit">
+            Отправить
+          </button>
+        </Form>
+      )}
+    </Formik>
+  )
 }
-
 export default App;
